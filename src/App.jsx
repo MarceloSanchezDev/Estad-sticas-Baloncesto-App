fe56620282f81e0669a592b982a84d0fcc6d8b0c
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-02-21 12:53:43 -0300

    add Login and Register Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import "./App.css";
+import Login from "./components/Login";
 import Main from "./components/Main";
+import Register from "./components/Register";
 
 function App() {
-  return <Main />;
+  return (
+    <>
+      <Main />
+      <Login />
+      <Register />
+    </>
+  );
 }
 
 export default App;
