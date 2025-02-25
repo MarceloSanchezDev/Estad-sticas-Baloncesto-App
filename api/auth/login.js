4b5f66884b4927d54519b40e4e7b279fbe6f2a0a
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-02-25 18:50:30 -0300

    test 1 of login logic

--- api/auth/login.js
+++ api/auth/login.js
@@ -8,6 +8,10 @@ export default async function handler(req, res) {
     if (req.method !== "POST") {
         return res.status(405).json({ error: "Método no permitido" });
     }
+    const { email, password } = req.body;
+    if(email & password){
+        return res.status(200).json({ message: 'Estadísticas de baloncesto', email, password });
+    }
     try {
         const { email, password } = req.body;
         const result = validUser({ email, password });
