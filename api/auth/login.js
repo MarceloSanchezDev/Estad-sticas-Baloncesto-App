5cb4809efd7cb7a2957a219f045f2a162d1b110a
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-02-25 18:56:22 -0300

    test 2 login logic

--- api/auth/login.js
+++ api/auth/login.js
@@ -1,6 +1,7 @@
+/*
 import { UserModel } from '../models/turso/userStatics.js';
 import { validUser } from '../schema/userSchema.js';
-import jwt from 'jsonwebtoken';
+import jwt from 'jsonwebtoken'; */
 
 const SECRET_KEY = process.env.SECRET_KEY;
 
@@ -12,7 +13,7 @@ export default async function handler(req, res) {
     if(email & password){
         return res.status(200).json({ message: 'Estadísticas de baloncesto', email, password });
     }
-    try {
+/*    try {
         const { email, password } = req.body;
         const result = validUser({ email, password });
         const userValid = await UserModel.login({ input: result.data });
@@ -27,5 +28,5 @@ export default async function handler(req, res) {
         return res.json({ email: userEmail, token, id });
     } catch (error) {
         return res.status(500).json({ errorServerless: `Error ${error.name}`, error: error.message });
-    }
+    } */
 }
