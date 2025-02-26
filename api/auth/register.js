c02d57376663eac9e0096be87583ed26f1b5f619
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-02-26 02:35:01 -0300

    add logic register endpoint

--- api/auth/register.js
+++ api/auth/register.js
@@ -1,8 +1,15 @@
+import { validRegisterUser } from "../schema/userSchema";
+
 export default function handler(req, res) {
     if (req.method !== "POST") {
         return res.status(405).json({ error: "Método no permitido" });
       }
-    const { email, username, name, lastname, password } = req.body;
+      try {
+        const result = validRegisterUser(req.body)
+        res.status(200).json({ message: 'Estadísticas de baloncesto', result});
+      } catch (error) {
+        res.status(500).json({ error: "Error en el servidor", message: error.message });
+      }
   
-    res.status(200).json({ message: 'Estadísticas de baloncesto', email, username, name, lastname, password});
+    
   } 
