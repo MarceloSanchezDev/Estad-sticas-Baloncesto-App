06f16fb062e976423415d1f9bf74dc292455d7aa
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-03-06 12:41:38 -0300

    add newStatistic component

--- src/components/NewStatistic.jsx
+++ src/components/NewStatistic.jsx
@@ -1,7 +1,137 @@
-export default function AllStatisticPercentage({ token, logout }) {
+import { useState } from "react";
+
+export default function AllStatisticPercentage({ token }) {
+  const [statistic, setStatistic] = useState({
+    lanzamientos3: 0,
+    encestados3: 0,
+    lanzamientos2: 0,
+    encestados2: 0,
+    libresLanzados: 0,
+    libresEncestados: 0,
+  });
+  const handlerNewStatistic = (e) => {
+    e.preventDefault();
+    console.log("Nueva Estadistica", statistic);
+    console.log(
+      "Estadisticas de 3:",
+      formula(statistic.lanzamientos3, statistic.encestados3)
+    );
+    console.log(
+      "Estadisticas de 2:",
+      formula(statistic.lanzamientos2, statistic.encestados2)
+    );
+    console.log(
+      "Estadisticas de Libres:",
+      formula(statistic.libresLanzados, statistic.libresEncestados)
+    );
+  };
+  const formula = (lanzados, encestados) => {
+    return (encestados / lanzados) * 100 + "%";
+  };
+
   return (
-    <div className="container text-center h-100 p-3 mt-5">
+    <div className="container text-center p-3 mt-5">
       <h1>Nueva Estadistica</h1>
+      <div className="bg-light d-flex justify-content-center flex-column p-3">
+        <form onSubmit={handlerNewStatistic}>
+          <div className="row">
+            <div className="mb-3 col">
+              <label htmlFor="input3Lanzados" className="form-label">
+                Cantidad de lanzamientos de 3 lanzados :
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="input3Lanzados"
+                placeholder="50"
+                onChange={(e) =>
+                  setStatistic({ ...statistic, lanzamientos3: e.target.value })
+                }
+              />
+            </div>
+            <div className="mb-3 col">
+              <label htmlFor="input3Encestados" className="form-label">
+                Cantidad de lanzamientos de 3 encestados :
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="input3Encestados"
+                placeholder="50"
+                onChange={(e) =>
+                  setStatistic({ ...statistic, encestados3: e.target.value })
+                }
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="mb-3 col">
+              <label htmlFor="input2Lanzados" className="form-label">
+                Cantidad de lanzamientos de 2 lanzados :
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="input2Lanzados"
+                placeholder="50"
+                onChange={(e) =>
+                  setStatistic({ ...statistic, lanzamientos2: e.target.value })
+                }
+              />
+            </div>
+            <div className="mb-3 col">
+              <label htmlFor="input2Encestados" className="form-label">
+                Cantidad de lanzamientos de 2 encestados :
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="input2Encestados"
+                placeholder="50"
+                onChange={(e) =>
+                  setStatistic({ ...statistic, encestados2: e.target.value })
+                }
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="mb-3 col">
+              <label htmlFor="inputLibresLanzados" className="form-label">
+                Cantidad de lanzamientos libres lanzados :
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="inputLibresLanzados"
+                placeholder="50"
+                onChange={(e) =>
+                  setStatistic({ ...statistic, libresLanzados: e.target.value })
+                }
+              />
+            </div>
+            <div className="mb-3 col">
+              <label htmlFor="inputLibresEncestados" className="form-label">
+                Cantidad de lanzamientos libres encestados :
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="inputLibresEncestados"
+                placeholder="50"
+                onChange={(e) =>
+                  setStatistic({
+                    ...statistic,
+                    libresEncestados: e.target.value,
+                  })
+                }
+              />
+            </div>
+          </div>
+          <button className="btn btn-outline-dark btn-lg">
+            Crear Nueva Estadistica
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -52,169 +52,168 @@ export default function Profile({ token, user }) {
       });
   };
   return (
-    <div className="d-flex vh-100 aparecer">
-      <div className="container text-center h-100 p-3 mt-5">
-        <div className="row h-100 ">
-          <div className="col h-100 p-3 m-2">
-            <div className="row h-100 d-flex flex-column justify-content-between">
-              <div
-                className="h-100 bg-light border rounded shadow-sm p-3"
-                style={{ minHeight: "720px" }}
-              >
-                <img
-                  className="img-fluid rounded-circle img-thumbnail mx-auto d-block user-select-none imgs"
-                  style={{ width: "250px" }}
-                  src={unknown}
-                  alt="unknown"
-                />
-                <div className="mt-3">
-                  <ul className="list-group">
-                    <li className="list-group-item d-flex justify-content-between align-items-start">
-                      <div className="ms-2 me-auto">
-                        <div className="fw-bold text-muted text-start">
-                          Nombre
-                        </div>
-                        <p className="text-start">
-                          {user.nombre
-                            ? `${user.nombre} ${user.apellido}`
-                            : "none"}
-                        </p>
+    <div className="container text-center h-100 p-3 mt-5 aparecer">
+      <h1>Perfil</h1>
+      <div className="row h-100 ">
+        <div className="col h-100 p-3 m-2">
+          <div className="row h-100 d-flex flex-column justify-content-between">
+            <div
+              className="h-100 bg-light border rounded shadow-sm p-3"
+              style={{ minHeight: "720px" }}
+            >
+              <img
+                className="img-fluid rounded-circle img-thumbnail mx-auto d-block user-select-none imgs"
+                style={{ width: "250px" }}
+                src={unknown}
+                alt="unknown"
+              />
+              <div className="mt-3">
+                <ul className="list-group">
+                  <li className="list-group-item d-flex justify-content-between align-items-start">
+                    <div className="ms-2 me-auto">
+                      <div className="fw-bold text-muted text-start">
+                        Nombre
                       </div>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between align-items-start">
-                      <div className="ms-2 me-auto">
-                        <div className="fw-bold text-muted text-start">
-                          E-mail
-                        </div>
-                        <p className="text-start">
-                          {user.email ? user.email : "none"}
-                        </p>
+                      <p className="text-start">
+                        {user.nombre
+                          ? `${user.nombre} ${user.apellido}`
+                          : "none"}
+                      </p>
+                    </div>
+                  </li>
+                  <li className="list-group-item d-flex justify-content-between align-items-start">
+                    <div className="ms-2 me-auto">
+                      <div className="fw-bold text-muted text-start">
+                        E-mail
                       </div>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between align-items-start">
-                      <div className="ms-2 me-auto">
-                        <div className="fw-bold text-muted text-start">
-                          Usuario
-                        </div>
-                        <p className="text-start">
-                          {user.username ? user.username : "none"}
-                        </p>
+                      <p className="text-start">
+                        {user.email ? user.email : "none"}
+                      </p>
+                    </div>
+                  </li>
+                  <li className="list-group-item d-flex justify-content-between align-items-start">
+                    <div className="ms-2 me-auto">
+                      <div className="fw-bold text-muted text-start">
+                        Usuario
                       </div>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between align-items-start">
-                      <div className="ms-2 me-auto w-100">
-                        <div className="fw-bold text-muted text-start">
-                          Posicion
-                        </div>
-                        <div className="d-flex justify-content-between align-items-start">
-                          {truePosition ? (
-                            <form className="d-flex" onSubmit={handlePosition}>
-                              <input
-                                type="text"
-                                className="form-control me-2"
-                                onChange={(e) => setNewPosition(e.target.value)}
-                              />
-                              <button
-                                type="submit"
-                                className="btn btn-outline-success"
-                              >
-                                Editar
-                              </button>
-                            </form>
-                          ) : (
-                            <p className="text-start">
-                              {user.posicion ? user.posicion : "none"}
-                            </p>
-                          )}
-                        </div>
+                      <p className="text-start">
+                        {user.username ? user.username : "none"}
+                      </p>
+                    </div>
+                  </li>
+                  <li className="list-group-item d-flex justify-content-between align-items-start">
+                    <div className="ms-2 me-auto w-100">
+                      <div className="fw-bold text-muted text-start">
+                        Posicion
                       </div>
-                      <button
-                        className="btn btn-light"
-                        onClick={() => editPosition()}
-                      >
+                      <div className="d-flex justify-content-between align-items-start">
                         {truePosition ? (
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="16"
-                            height="16"
-                            fill="currentColor"
-                            className="bi bi-x-circle-fill"
-                            viewBox="0 0 16 16"
-                          >
-                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z" />
-                          </svg>
+                          <form className="d-flex" onSubmit={handlePosition}>
+                            <input
+                              type="text"
+                              className="form-control me-2"
+                              onChange={(e) => setNewPosition(e.target.value)}
+                            />
+                            <button
+                              type="submit"
+                              className="btn btn-outline-success"
+                            >
+                              Editar
+                            </button>
+                          </form>
                         ) : (
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="16"
-                            height="16"
-                            fill="currentColor"
-                            className="bi bi-pencil"
-                            viewBox="0 0 16 16"
-                          >
-                            <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
-                          </svg>
+                          <p className="text-start">
+                            {user.posicion ? user.posicion : "none"}
+                          </p>
                         )}
-                      </button>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between align-items-start">
-                      <div className="ms-2 me-auto w-100">
-                        <div className="fw-bold text-muted text-start">
-                          Categoria
-                        </div>
-                        <div className="d-flex justify-content-between align-items-start">
-                          {trueCategory ? (
-                            <form className="d-flex" onSubmit={handleCategory}>
-                              <input
-                                type="text"
-                                className="form-control me-2"
-                                onChange={(e) => setNewCategory(e.target.value)}
-                              />
-                              <button
-                                type="submit"
-                                className="btn btn-outline-success"
-                              >
-                                Editar
-                              </button>
-                            </form>
-                          ) : (
-                            <p className="text-start">
-                              {user.categoria ? user.categoria : "none"}
-                            </p>
-                          )}
-                        </div>
                       </div>
-                      <button
-                        className="btn btn-light"
-                        onClick={() => editCategory()}
-                      >
+                    </div>
+                    <button
+                      className="btn btn-light"
+                      onClick={() => editPosition()}
+                    >
+                      {truePosition ? (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          className="bi bi-x-circle-fill"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z" />
+                        </svg>
+                      ) : (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          className="bi bi-pencil"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
+                        </svg>
+                      )}
+                    </button>
+                  </li>
+                  <li className="list-group-item d-flex justify-content-between align-items-start">
+                    <div className="ms-2 me-auto w-100">
+                      <div className="fw-bold text-muted text-start">
+                        Categoria
+                      </div>
+                      <div className="d-flex justify-content-between align-items-start">
                         {trueCategory ? (
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="16"
-                            height="16"
-                            fill="currentColor"
-                            className="bi bi-x-circle-fill"
-                            viewBox="0 0 16 16"
-                          >
-                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z" />
-                          </svg>
+                          <form className="d-flex" onSubmit={handleCategory}>
+                            <input
+                              type="text"
+                              className="form-control me-2"
+                              onChange={(e) => setNewCategory(e.target.value)}
+                            />
+                            <button
+                              type="submit"
+                              className="btn btn-outline-success"
+                            >
+                              Editar
+                            </button>
+                          </form>
                         ) : (
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="16"
-                            height="16"
-                            fill="currentColor"
-                            className="bi bi-pencil"
-                            viewBox="0 0 16 16"
-                          >
-                            <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
-                          </svg>
+                          <p className="text-start">
+                            {user.categoria ? user.categoria : "none"}
+                          </p>
                         )}
-                      </button>
-                    </li>
-                  </ul>
-                </div>
+                      </div>
+                    </div>
+                    <button
+                      className="btn btn-light"
+                      onClick={() => editCategory()}
+                    >
+                      {trueCategory ? (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          className="bi bi-x-circle-fill"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z" />
+                        </svg>
+                      ) : (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          className="bi bi-pencil"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
+                        </svg>
+                      )}
+                    </button>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
