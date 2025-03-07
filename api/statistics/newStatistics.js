8b0df999c7e0fd1e99f5e6a4de7b81c1349defd1
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-03-07 17:55:50 -0300

    add titulo to response in newStatistics API handler

--- api/statistics/newStatistics.js
+++ api/statistics/newStatistics.js
@@ -35,6 +35,7 @@ export default async function handler(req, res) {
             porcentaje3Puntos,
             porcentajeLibres,
             fecha,
+            titulo,
             hora, username}) 
             if (!result || !result.data) {
                 console.warn("⚠️ Datos de usuario inválidos");
