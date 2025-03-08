2efeaed6e581b1967d9addf3a0d4fa56ad975236
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-03-08 04:06:05 -0300

    fix: add conditional rendering for Doughnut chart in AllStatisticPercentage component

--- src/components/AllStatisticPercentage.jsx
+++ src/components/AllStatisticPercentage.jsx
@@ -51,21 +51,23 @@ export default function AllStatisticPercentage({ token, user }) {
   return (
     <div className="container text-center h-100 p-3 mt-5">
       <h1>Porcentaje Total</h1>
-      <Doughnut
-        data={{
-          labels: ["Encestados", "Fallados"],
-          datasets: [
-            {
-              label: "Distribución de Lanzamientos",
-              data: [
-                allPorcentages.total_encestados,
-                allPorcentages.total_tiros,
-              ],
-              backgroundColor: ["rgb(255, 99, 132)", "rgb(54, 162, 235)"],
-            },
-          ],
-        }}
-      />
+      {allPorcentages && (
+        <Doughnut
+          data={{
+            labels: ["Encestados", "Fallados"],
+            datasets: [
+              {
+                label: "Distribución de Lanzamientos",
+                data: [
+                  allPorcentages.total_encestados,
+                  allPorcentages.total_tiros,
+                ],
+                backgroundColor: ["rgb(255, 99, 132)", "rgb(54, 162, 235)"],
+              },
+            ],
+          }}
+        />
+      )}
     </div>
   );
 }
