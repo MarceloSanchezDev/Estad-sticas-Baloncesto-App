accade59a51bc867b2114e586d0bc8d952403203
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-03-08 13:07:43 -0300

    fix: limit max height of container in AllStatisticPercentage component for better layout

--- src/components/AllStatisticPercentage.jsx
+++ src/components/AllStatisticPercentage.jsx
@@ -51,7 +51,7 @@ export default function AllStatisticPercentage({ token, user }) {
     <div className="container text-center h-100 p-3 mt-5">
       <h1>Porcentaje Total</h1>
       {allPorcentages ? (
-        <div className="container">
+        <div className="container" style={{ maxHeight: "50vh" }}>
           <Doughnut
             data={{
               labels: ["Encestados", "Lanzados"],
