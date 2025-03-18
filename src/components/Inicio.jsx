724cc7402b883a6029d0d1c01844b7a7653cbff6
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-03-18 12:34:15 -0300

    feat: add loading state with Loader component in Inicio for better user experience

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import StatInit from "../components/StatInit";
 import { useNavigate } from "react-router";
+import Loader from "./loaders/Loader";
 export default function Inicio({ token, user }) {
   const [info, setInfo] = useState();
   const navigate = useNavigate();
@@ -29,15 +30,21 @@ export default function Inicio({ token, user }) {
   }, [user.username]);
   return (
     <div className="container text-center aparecer d-flex flex-column align-items-center justify-content-center ">
-      <h1 className="mt-1">Inicio</h1>
-      <div
-        className="row rounded-3 d-flex align-items-center justify-content-between"
-        style={{ height: "750px", width: "100%" }}
-      >
-        <StatInit titulo={"Lanzamientos totales de 3 puntos"} info={info} />
-        <StatInit titulo={"Lanzamientos totales de 2 puntos"} info={info} />
-        <StatInit titulo={"Lanzamientos totales de Libres"} info={info} />
-      </div>
+      {user ? (
+        <>
+          <h1 className="mt-1">Inicio</h1>
+          <div
+            className="row rounded-3 d-flex align-items-center justify-content-between"
+            style={{ height: "750px", width: "100%" }}
+          >
+            <StatInit titulo={"Lanzamientos totales de 3 puntos"} info={info} />
+            <StatInit titulo={"Lanzamientos totales de 2 puntos"} info={info} />
+            <StatInit titulo={"Lanzamientos totales de Libres"} info={info} />
+          </div>
+        </>
+      ) : (
+        <Loader />
+      )}
     </div>
   );
 }
