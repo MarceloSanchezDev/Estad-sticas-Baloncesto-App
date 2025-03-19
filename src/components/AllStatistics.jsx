d9b0e039c4f42cc3047beb10b4d90ad837e6010c
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-03-19 12:23:32 -0300

    fix: remove unnecessary console log and correct length check in AllStatisticPercentage component

--- src/components/AllStatistics.jsx
+++ src/components/AllStatistics.jsx
@@ -27,7 +27,6 @@ ChartJS.register(
 export default function AllStatisticPercentage({ token, user }) {
   const [allStatistics, setAllStatistics] = useState();
   const navigate = useNavigate();
-
   useEffect(() => {
     if (!token) {
       navigate("/");
@@ -42,7 +41,7 @@ export default function AllStatisticPercentage({ token, user }) {
           body: JSON.stringify({ username: user.username }),
         });
         const data = await result.json();
-        console.log(data.response);
+
         if (!result.ok) {
           Swal.fire({
             position: "bottom-end",
@@ -83,87 +82,87 @@ export default function AllStatisticPercentage({ token, user }) {
     <div className="container text-center aparecer">
       {allStatistics ? (
         <div className="container">
-          {allStatistics.legth >= 1 ? (
-            <div className="row">
-              {allStatistics.map((e) => (
-                <div
-                  key={e.id_stat}
-                  className="col-sm-6 col-xs-12 col-lg-4 my-3 "
-                >
-                  <div className="card  border-primary border rounded-3 shadow-lg">
-                    <div className="card-header">
-                      <h5 className="card-title">{e.nombreEstadistica}</h5>
-                      <h6 className="card-subtitle mb-2 text-body-secondary">
-                        {` Fecha :${e.fecha} Hora :${e.hora}`}
-                      </h6>
-                    </div>
-                    <div className="card-body">
-                      <Doughnut
-                        key={e.id_stat}
-                        data={{
-                          labels: ["Encestados", "Fallados"],
-                          datasets: [
-                            {
-                              label: "Distribución de Lanzamientos",
-                              data: [
-                                e.cantLibresEncestados +
+          <div className="row">
+            {allStatistics.map((e) => (
+              <div
+                key={e.id_stat}
+                className="col-sm-6 col-xs-12 col-lg-4 my-3 "
+              >
+                <div className="card  border-primary border rounded-3 shadow-lg">
+                  <div className="card-header">
+                    <h5 className="card-title">{e.nombreEstadistica}</h5>
+                    <h6 className="card-subtitle mb-2 text-body-secondary">
+                      {` Fecha :${e.fecha} Hora :${e.hora}`}
+                    </h6>
+                  </div>
+                  <div className="card-body">
+                    <Doughnut
+                      key={e.id_stat}
+                      data={{
+                        labels: ["Encestados", "Fallados"],
+                        datasets: [
+                          {
+                            label: "Distribución de Lanzamientos",
+                            data: [
+                              e.cantLibresEncestados +
+                                e.cant_tresPuntosEncestados +
+                                e.cant_dosPuntosEncestados,
+
+                              e.cant_tresPuntos +
+                                e.cant_dosPuntos +
+                                e.cantLibres -
+                                (e.cantLibresEncestados +
                                   e.cant_tresPuntosEncestados +
-                                  e.cant_dosPuntosEncestados,
+                                  e.cant_dosPuntosEncestados),
+                            ],
+                            backgroundColor: ["#36A2EB", "#FF6384"],
+                          },
+                        ],
+                      }}
+                    />
+                  </div>
+                  <ul className="list-group list-group-flush rounded">
+                    <li className="list-group-item text-bg-info">
+                      Porcentaje Lanzamientos de 3 puntos :{" "}
+                      {e.estadisticasTresPuntos}
+                    </li>
+                    <li className="list-group-item text-bg-info">
+                      Porcentaje de 2 puntos : {e.estadisticasDosPuntos}
+                    </li>
 
-                                e.cant_tresPuntos +
-                                  e.cant_dosPuntos +
-                                  e.cantLibres -
-                                  (e.cantLibresEncestados +
-                                    e.cant_tresPuntosEncestados +
-                                    e.cant_dosPuntosEncestados),
-                              ],
-                              backgroundColor: ["#36A2EB", "#FF6384"],
-                            },
-                          ],
-                        }}
-                      />
-                    </div>
-                    <ul className="list-group list-group-flush rounded">
-                      <li className="list-group-item text-bg-info">
-                        Porcentaje Lanzamientos de 3 puntos :{" "}
-                        {e.estadisticasTresPuntos}
-                      </li>
-                      <li className="list-group-item text-bg-info">
-                        Porcentaje de 2 puntos : {e.estadisticasDosPuntos}
-                      </li>
-
-                      <li className="list-group-item text-bg-info">
-                        Porcentaje de tiro Libre : {e.estadisticasLibres}
-                      </li>
-                    </ul>
-                    <div className="p-2">
-                      <Link
-                        to={`/infoStat?StatID=${e.id_stat}`}
-                        className="btn btn-success"
-                      >
-                        Ver Estadistica
-                      </Link>
-                    </div>
+                    <li className="list-group-item text-bg-info">
+                      Porcentaje de tiro Libre : {e.estadisticasLibres}
+                    </li>
+                  </ul>
+                  <div className="p-2">
+                    <Link
+                      to={`/infoStat?StatID=${e.id_stat}`}
+                      className="btn btn-success"
+                    >
+                      Ver Estadistica
+                    </Link>
                   </div>
                 </div>
-              ))}
-            </div>
-          ) : (
-            <div className="row">
-              <h1 className="text-white">Aun no tienes Ninguna Estadistica</h1>
-            </div>
-          )}
+              </div>
+            ))}
+          </div>
         </div>
       ) : (
         <div className="container vh-100 d-flex d-flex align-items-center justify-content-center">
-          <div className="row d-flex align-items-center justify-content-center py-3 px-3">
-            <StatLoader />
-            <StatLoader />
-            <StatLoader />
-            <StatLoader />
-            <StatLoader />
-            <StatLoader />
-          </div>
+          {!allStatistics || allStatistics.length > 0 ? (
+            <div className="row">
+              <h1 className="text-white">Aun no tienes Ninguna Estadistica</h1>
+            </div>
+          ) : (
+            <div className="row d-flex align-items-center justify-content-center py-3 px-3">
+              <StatLoader />
+              <StatLoader />
+              <StatLoader />
+              <StatLoader />
+              <StatLoader />
+              <StatLoader />
+            </div>
+          )}
         </div>
       )}
     </div>
