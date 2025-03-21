7b0debca47e331d0564d8138475e5bafec05e30b
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-03-21 11:57:57 -0300

    feat: add StatisticsContext for centralized statistics management and refactor Inicio component to utilize new context

---
+++ src/context/StatisticContext.jsx
@@ -0,0 +1,37 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { useAuth } from "./AuthContext"; // Usamos AuthContext para obtener el usuario
+
+const StatisticsContext = createContext();
+
+export const StatisticsProvider = ({ children }) => {
+  const { user } = useAuth();
+  const [stats, setStats] = useState(null);
+
+  useEffect(() => {
+    if (!user?.username) return;
+
+    async function fetchStatistics() {
+      try {
+        const result = await fetch(`/api/statistics/allInfo`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ username: user.username }),
+        });
+        const data = await result.json();
+        setStats(data.response[0]);
+      } catch (error) {
+        console.error("Error fetching statistics:", error);
+      }
+    }
+
+    fetchStatistics();
+  }, [user]);
+
+  return (
+    <StatisticsContext.Provider value={{ stats, setStats }}>
+      {children}
+    </StatisticsContext.Provider>
+  );
+};
+
+export const useStatistics = () => useContext(StatisticsContext);
